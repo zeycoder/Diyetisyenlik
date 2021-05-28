f55f1ce0f274dcf63a0ed8227386a63103a05ab8
zeyneparslan55 <58356584+zeyneparslan55@users.noreply.github.com>
2021-05-28 15:09:15 +0300

    Add project files.

---
+++ Diyetisyenlik/Controllers/KayitGirisController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diyetisyenlik.Controllers
+{
+    public class KayitGirisController : Controller
+    {
+        // GET: KayitGiris
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult KayitEkle()
+        {
+            ViewBag.Message = "KAYIT EKLE SAYFAMIZ";
+            return View();
+        }
+        public ActionResult Giris()
+        {
+            ViewBag.Message = "GİRİS YAP SAYFAMIZ";
+            return View();
+        }
+    }
+}
